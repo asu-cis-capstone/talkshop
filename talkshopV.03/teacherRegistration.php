57cbb4777ad02309e31728c988044e1cad809911
Katie Kramer <klkrame2@asu.edu>
2015-02-24 16:11:09 -0700

    New Teacher Registration Form

--- talkshopV.03/teacherRegistration.php
+++ talkshopV.03/teacherRegistration.php
@@ -41,6 +41,21 @@ TALK SHOP! Teacher Registration Form
 	
     <!-- Web Page Title -->
     <title>TalkShop | Registration</title>
+	
+	<!-- JavaScript Tags -->
+	<script type="text/javascript" language="JavaScript">
+		function HidePart(d) { document.getElementById(d).style.display = "none";  }
+		function ShowPart(d) { document.getElementById(d).style.display = "block"; }
+		function CheckboxChecked(b,d)
+			{
+				if(b) { ShowPart(d); }
+				else  { HidePart(d); }
+			}
+	</script>
+	
+	<script type="text/javascript">
+		CheckboxChecked(document.myform.mycheckbox.checked,'checkboxdiv');;
+	</script>
 
   </head>
 
@@ -51,7 +66,9 @@ TALK SHOP! Teacher Registration Form
 	<div id="teacherRegistrationTitle">TEACHER REGISTRATION</div>
 	
 	<br />
-	
+
+
+
 	<form id="joinform" action="../confirm.htm" method="get">
 				<p>
 					<!-- first name -->
@@ -162,6 +179,13 @@ TALK SHOP! Teacher Registration Form
 					<!--<label for="phone">Phone Number:</label>-->
 					<input placeholder="Phone Number" type="number" id="phone" name="phone" max= "10" />
 					<br />
+
+					<!-- make active profile button -->
+					Make active profile?
+					<input type="checkbox" name="mycheckbox" value="yes" 
+						onclick="CheckboxChecked(this.checked,'checkboxdiv')">				
+					
+				<div id="checkboxdiv" style="display:none">
 					
 					<!-- age group -->
 					<!--<label for="agregroup">Representing Age Group:</label>-->
@@ -173,27 +197,59 @@ TALK SHOP! Teacher Registration Form
 						<option value="high">High School</option>
 						<option value="college">College</option>
 						<option value="other">Other</option>
-					</select>	
-				</p>
-				
-				<form action="uploadImage.php" method="post" enctype="multipart/form-data">
+					</select> 
+					<br />
+					
+					<!-- topic areas -->
+						<p>Select Topic Areas</p>
+						<input type="checkbox" name="" value="">Agriculture
+						<input type="checkbox" name="" value="">Art
+						<input type="checkbox" name="" value="">Biological Sciences
+						<input type="checkbox" name="" value="">Business
+						<input type="checkbox" name="" value="">Communication
+						<input type="checkbox" name="" value="">Computers/Information Technology
+						<input type="checkbox" name="" value="">Education
+						<input type="checkbox" name="" value="">Engineering
+						<input type="checkbox" name="" value="">Environmental Science
+						<input type="checkbox" name="" value="">Health
+						<input type="checkbox" name="" value="">Language and Literature
+						<input type="checkbox" name="" value="">Law
+						<input type="checkbox" name="" value="">Math
+						<input type="checkbox" name="" value="">Philosophy and Religion
+						<input type="checkbox" name="" value="">Physical Science
+						<input type="checkbox" name="" value="">Psychology and Counseling
+						<input type="checkbox" name="" value="">Recreation and Fitness
+						<input type="checkbox" name="" value="">Skilled Trade and Construction
+						<input type="checkbox" name="" value="">Social Sciences and Liberal Arts
+						<input type="checkbox" name="" value="">Social Services
+						<input type="checkbox" name="" value="">Transportation
+						<input type="checkbox" name="" value="">Other
+					
+					<!-- biography -->
+					<br />
+					<textarea placeholder="Bio and Additional Information" id="comments" name="comments" 
+						rows="2" cols="49"></textarea>
+					<br />
+					
+					<!-- upload image -->
+					<form action="uploadImage.php" method="post" enctype="multipart/form-data" style="display:none">
 						Upload Profile Image
 						<br/>
 						<input type="file" name="fileToUpload" id="fileToUpload">
 						<br/>
 						<input type="submit" value="Upload Image" name="submit">
-						<br/>
+						<br/>	
 						
 					<p class="submit">
-						<input type="submit" value="REGISTER"
+					<input type="submit" value="REGISTER"
 						onclick="return a3Validate()"/>
-						<!--<span class="reset">
-							<input type="reset" value="Clear" onclick="a3Focus()" />
-						</span>-->
 					</p>
-				</form>
-				
+				</p>
+					</form>
+				</div>	
+					
 		</form>
+	
 	
 	<?php include 'bottomBarHome.php' ?>
 	
