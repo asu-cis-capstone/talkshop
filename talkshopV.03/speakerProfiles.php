52ecab771e469e039d83c0831c8bd27a6e2a00a2
Ivette Estrada <ivette.estrada@asu.edu>
2015-02-24 15:14:04 -0700

    Updated Speaker Profiles
    updated speakerProfile and speakerProfile css

--- talkshopV.03/speakerProfiles.php
+++ talkshopV.03/speakerProfiles.php
@@ -3,8 +3,6 @@ TALK SHOP! Speaker Profile Template (hard coded)
 -->
 
 <?php include 'htmlHeader.php' ?>
-
-  <head>
   
 	<!-- Link tag for speakerProfile CSS -->
     <link type="text/css" rel="stylesheet" href="style/speakerProfile.css" />
@@ -22,23 +20,27 @@ TALK SHOP! Speaker Profile Template (hard coded)
 	
 		<img src="images/joeclark.png" alt="Profile Image" height="200" width="200">
 		
-		<p id="speakerName">Dr. Joseph Clark</p>
+		<p id="speakerName">Joseph Clark</p>
 		
-		<p id="backToSearch"><a href="search.php">Search More Speakers</a></p>
+		<!--<p id="backToSearch"><a href="search.php">Search More Speakers</a></p>-->
 		
 		<p id="profession">Profession: Information Technology</p>
 		
 		<p id="topicAreas">Topic Areas: IT, Entrepreneurship, Education, Business, Analytics </p>
 		
+		<p id="ageGroup">Target Audience: College, High School</p>
+		
 		<p id="geographicLocation">Location: Phoenix, AZ</p>	
 		
-		<p id="aboutSpeaker">About Dr. Joseph Clark:</p>
+		<p id="aboutSpeaker">About Joseph Clark:</p>
 		
 		<p id="socialMediaLinks"><a href="https://facebook.com" onclick="window.open(this.href) ; return false;"><img src="images/facebook.png" alt="Facebook" height="25" width="25"></a>
 		<a href="https://twitter.com" onclick="window.open(this.href) ; return false;"><img src="images/twitter.png" alt="Twitter" height="25" width="25"></a>
 		<a href="https://plus.google.com" onclick="window.open(this.href) ; return false;"><img src="images/google.png" alt="GooglePlus" height="25" width="25"></a>
 		<a href="https://linkedin.com" onclick="window.open(this.href) ; return false;"><img src="images/linkedin.png" alt="Linkedin" height="25" width="25"></a>
 		</p>
+		
+		<p id="websiteURL">joeclark.net</p>
 		
 		<p id="aboutParagraph">Joseph Clark is a Clinical Assistant Professor in the Information Systems department of Arizona State University’s 
 		W. P. Carey School of Business. He received his PhD in Information Systems from the University of Southern California’s Marshall School 
@@ -51,7 +53,9 @@ TALK SHOP! Speaker Profile Template (hard coded)
 		for several innovative companies including NBC and Disney before the first dot-com bubble burst in 2001. His teaching areas include data 
 		analysis and visualization in business intelligence, data stream driven decision making, and emerging digital business models.</p>
 		
-		<p id="contactSpeaker"><a href="login.php">Contact Joe Clark</a></p>
+		<div id="contactButton" onclick="javascript:location.href='login.php'">
+		<div id="contactSpeaker"><a href="login.php">Contact Joe Clark</a></div>
+		</div>
 
 	</div>
 	
