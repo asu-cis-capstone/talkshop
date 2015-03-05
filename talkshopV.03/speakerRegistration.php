1695c2bc17db08199b9d193106051a4c5a1e0e4e
Jonathan Nazzaro <jonathannazzaro1@MacBook-Pro.local>
2015-03-05 12:30:15 -0700

    added php

--- talkshopV.03/speakerRegistration.php
+++ talkshopV.03/speakerRegistration.php
@@ -1,3 +1,35 @@
+<?php
+    /*
+     include('../local-connect.php');
+     $fname1 = $_POST['firstname'];
+     $fname2 = mysqli_real_escape_string($dbc,$fname1);
+     $lname1 = $_POST['lastname'];
+     $lname2 = mysqli_real_escape_string($dbc,$lname1);
+     $username = $_POST['username'];
+     $email = $_POST['email'];
+     $password = $_POST['password'];
+     $org = $_POST['organization'];
+     $line1 = $_POST['lineone'];
+     $line2 = $_POST['linetwo'];
+     $city = $_POST['city'];
+     $state = $_POST['state'];
+     $zip = $_POST['zip'];
+     $phone = $_POST['phone'];
+     $age = $_POST['agegroup'];
+     $image = $_POST['fileToUpload'];
+     
+     
+     $query = "INSERT INTO teachers(firstname, lastname, username, email, password, organization, addressone, addresstwo, city, state, zip, phone, agegroup, image)" .  "VALUES('$fname2','$lname2','$username', '$email','$password','$org', '$line1','$line2','$city', '$state', '$zip', '$phone','$age', '$image')";
+     
+     $result = mysqli_query($dbc, $query) or die('Unable to Connect to Database or the Registration is incomplete!');
+     
+     mysqli_close($dbc);
+     */
+?>
+
+
+
+
 <?php include 'htmlHeader.php' ?>
 
 	<!-- Link tag for teacherRegistration CSS -->
