71860a12345a7c0bafd50409378f7bebb353461f
Ivette Estrada <ivette.estrada@asu.edu>
2015-03-19 15:34:38 -0700

    Revert "Fixing my github :("
    This reverts commit 402f5aa9d2a152f58532caab14c993aafb691edb.

--- talkshopV.04/speakerProfiles.php
+++ talkshopV.04/speakerProfiles.php
@@ -19,173 +19,105 @@ TALK SHOP! Speaker Profile Template (hard coded)
 	
 	<div id="searchForm">
 	
-		<p id="searchTitle">Search</p>
-		
-			<!--<form method="post" action="search.php?go"  id="searchform">-->
-			
-			<!--<input class="textFields" placeholder="Search" type="text" id="search" name="search">-->
-			
-			<form method="post" action="search/search_display.php?go" id="searchform">
-		<input type="text" name="name" class="textFields" placeholder="Search" type="text" id="search" name="search">
-		
-		
-		<select class="textFields" id="state" name="state">
-									<option value="">State</option>
-									<option value="AL">Alabama</option>
-									<option value="AK">Alaska</option>
-									<option value="AZ">Arizona</option>
-									<option value="AR">Arkansas</option>
-									<option value="CA">California</option>
-									<option value="CO">Colorado</option>
-									<option value="CT">Connecticut</option>
-									<option value="DE">Delaware</option>
-									<option value="DC">District of Columbia</option>
-									<option value="FL">Florida</option>
-									<option value="GA">Georgia</option>
-									<option value="HI">Hawaii</option>
-									<option value="ID">Idaho</option>
-									<option value="IL">Illinois</option>
-									<option value="IN">Indiana</option>
-									<option value="IA">Iowa</option>
-									<option value="KS">Kansas</option>
-									<option value="KY">Kentucky</option>
-									<option value="LA">Louisiana</option>
-									<option value="ME">Maine</option>
-									<option value="MD">Maryland</option>
-									<option value="MA">Massachusetts</option>
-									<option value="MI">Michigan</option>
-									<option value="MN">Minnesota</option>
-									<option value="MS">Mississippi</option>
-									<option value="MO">Missouri</option>
-									<option value="MT">Montana</option>
-									<option value="NE">Nebraska</option>
-									<option value="NV">Nevada</option>
-									<option value="NH">New Hampshire</option>
-									<option value="NJ">New Jersey</option>
-									<option value="NM">New Mexico</option>
-									<option value="NY">New York</option>
-									<option value="NC">North Carolina</option>
-									<option value="ND">North Dakota</option>
-									<option value="OH">Ohio</option>
-									<option value="OK">Oklahoma</option>
-									<option value="OR">Oregon</option>
-									<option value="PA">Pennsylvania</option>
-									<option value="RI">Rhode Island</option>
-									<option value="SC">South Carolina</option>
-									<option value="SD">South Dakota</option>
-									<option value="TN">Tennessee</option>
-									<option value="TX">Texas</option>
-									<option value="UT">Utah</option>
-									<option value="VT">Vermont</option>
-									<option value="VA">Virginia</option>
-									<option value="WA">Washington</option>
-									<option value="WV">West Virginia</option>
-									<option value="WI">Wisconsin</option>
-									<option value="WY">Wyoming</option>
-							</select>
-							
-							</br>
-							
-							<select class="textFields" id="searchtopicAreas" name="topicArea">
-									<option value="">Topic Areas</option>
-									<option value="AG">Agriculture</option>
-									<option value="BU">Business</option>
-									<option value="HE">Health</option>
-									<option value="PHR">Philosophy and Religion</option>
-									<option value="RF">Recreation and Fitness</option>
-									<option value="SS">Social Services</option>
-									<option value="AR">Art</option>
-									<option value="COM">Communication</option>
-									<option value="EN">Engineering</option>
-									<option value="LL">Language and Literature</option>
-									<option value="PS">Physical Science</option>
-									<option value="STC">Skilled Trade and Construction</option>
-									<option value="TR">Transportation</option>
-									<option value="BS">Biological Sciences</option>
-									<option value="IT">Information Technology</option>
-									<option value="ES">Environmental Science</option>
-									<option value="LA">Law</option>
-									<option value="PC">Psychology and Counseling</option>
-									<option value="SSLA">Social Sciences and Liberal Arts</option>
-							</select>
-							
-							</br>
-							
-							<select class="textFields" id="searchTargetAudience" name="searchTargetAudience">
-									<option value="">Target Audience</option>
-									<option value="PS">Pre School</option>
-									<option value="ES">Elementary</option>
-									<option value="MS">Middle School</option>
-									<option value="HS">High School</option>
-									<option value="CS">College</option>
-									
-							</select>
-		
-		<!--This is the search button!-->
-		<input type="submit" name="submit" value="Search" id="searchButton" >
-		
-	</form>
-<?php
-
-if(isset($_POST['submit'])){
-if(isset($_GET['go'])){
-if(preg_match("/[A-Z | a-z]+/", $_POST['name'])){
-$name=$_POST['name'];
-
-//connect to the database
-$db=mysql_connect ("localhost", "root", "") or die ('I cannot connect to the database because: ' . mysql_error()); 
-
-//-select the database to use
-$mydb=mysql_select_db("talkshop_ivettetest");
-
-
-//-query the database table
-$sql="SELECT FName, Lname, city, state, zip FROM users WHERE FName LIKE '%" . $name . "%' 
-OR Lname LIKE '%" . $name ."%' 
-OR city LIKE '%" . $name . "%' 
-OR state LIKE '%" . $name . "%'
-OR zip LIKE '%" . $name . "%'
-";
-
-//-run the query against the mysql query function
-$result=mysql_query($sql);
-
-//-create while loop and loop through result set
-while($row=mysql_fetch_array($result)){
-
-	$FirstName =$row['FName'];
-	$LastName=$row['Lname'];
-	$City=$row['city'];
-	$State=$row['state'];
-	$Zip=$row['zip'];
-	$ID=$row['UUID'];
-		
-//-display the result of the array
-
-echo "<ul>\n"; 
-echo "<li>" . "<a href=\"search_display.php?id=$ID\">"  .$FirstName . " " . $LastName . " " . $City . " " . $State. " " . $Zip . "</a></li>\n";
-echo "</ul>";
-}
-}
-else{
-echo "<p>Please enter a search query</p>";
-}
-}
-}
-?>
-			
-			<!--<input class="textFields" placeholder="Search" type="text" id="search" name="search">-->
-			
-			
-			
-			
-			
-							<!-- This is the styled searchButton-->
-							<!--<div id="searchButton" input type="submit" name="submit" value="Search" ">-->
-							<!--<div id="searchButtonText"><a href="search.php">Search</a></div>-->
+	<p id="search">Search</p>
+	
+	<form>
+	
+	<input class="textFields" placeholder="Search" type="text" id="search" name="search">
+	
+	<select class="textFields" id="state" name="state">
+							<option value="">State</option>
+							<option value="AL">Alabama</option>
+							<option value="AK">Alaska</option>
+							<option value="AZ">Arizona</option>
+							<option value="AR">Arkansas</option>
+							<option value="CA">California</option>
+							<option value="CO">Colorado</option>
+							<option value="CT">Connecticut</option>
+							<option value="DE">Delaware</option>
+							<option value="DC">District of Columbia</option>
+							<option value="FL">Florida</option>
+							<option value="GA">Georgia</option>
+							<option value="HI">Hawaii</option>
+							<option value="ID">Idaho</option>
+							<option value="IL">Illinois</option>
+							<option value="IN">Indiana</option>
+							<option value="IA">Iowa</option>
+							<option value="KS">Kansas</option>
+							<option value="KY">Kentucky</option>
+							<option value="LA">Louisiana</option>
+							<option value="ME">Maine</option>
+							<option value="MD">Maryland</option>
+							<option value="MA">Massachusetts</option>
+							<option value="MI">Michigan</option>
+							<option value="MN">Minnesota</option>
+							<option value="MS">Mississippi</option>
+							<option value="MO">Missouri</option>
+							<option value="MT">Montana</option>
+							<option value="NE">Nebraska</option>
+							<option value="NV">Nevada</option>
+							<option value="NH">New Hampshire</option>
+							<option value="NJ">New Jersey</option>
+							<option value="NM">New Mexico</option>
+							<option value="NY">New York</option>
+							<option value="NC">North Carolina</option>
+							<option value="ND">North Dakota</option>
+							<option value="OH">Ohio</option>
+							<option value="OK">Oklahoma</option>
+							<option value="OR">Oregon</option>
+							<option value="PA">Pennsylvania</option>
+							<option value="RI">Rhode Island</option>
+							<option value="SC">South Carolina</option>
+							<option value="SD">South Dakota</option>
+							<option value="TN">Tennessee</option>
+							<option value="TX">Texas</option>
+							<option value="UT">Utah</option>
+							<option value="VT">Vermont</option>
+							<option value="VA">Virginia</option>
+							<option value="WA">Washington</option>
+							<option value="WV">West Virginia</option>
+							<option value="WI">Wisconsin</option>
+							<option value="WY">Wyoming</option>
+					</select>
 					
+					<select class="textFields" id="searchtopicAreas" name="topicArea">
+							<option value="">Topic Areas</option>
+							<option value="AG">Agriculture</option>
+							<option value="BU">Business</option>
+							<option value="HE">Health</option>
+							<option value="PHR">Philosophy and Religion</option>
+							<option value="RF">Recreation and Fitness</option>
+							<option value="SS">Social Services</option>
+							<option value="AR">Art</option>
+							<option value="COM">Communication</option>
+							<option value="EN">Engineering</option>
+							<option value="LL">Language and Literature</option>
+							<option value="PS">Physical Science</option>
+							<option value="STC">Skilled Trade and Construction</option>
+							<option value="TR">Transportation</option>
+							<option value="BS">Biological Sciences</option>
+							<option value="IT">Information Technology</option>
+							<option value="ES">Environmental Science</option>
+							<option value="LA">Law</option>
+							<option value="PC">Psychology and Counseling</option>
+							<option value="SSLA">Social Sciences and Liberal Arts</option>
+					</select>
+					
+					<select class="textFields" id="searchTargetAudience" name="searchTargetAudience">
+							<option value="">Target Audience</option>
+							<option value="PS">Pre School</option>
+							<option value="ES">Elementary</option>
+							<option value="MS">Middle School</option>
+							<option value="HS">High School</option>
+							<option value="CS">College</option>
+							
+					</select>
+					
+					<div id="searchButton" onclick="javascript:location.href='search.php'">
+					<div id="searchButtonText"><a href="search.php">Search</a></div>
 					</div>
-		</form>
+				
+	</form>
 
 	</div>
 
